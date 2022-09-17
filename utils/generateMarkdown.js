9eb8ab9ad9b9252c906c1689153486f82fb78844
Val Pal <valgonzalez13@yahoo.com>
2022-09-17 17:31:29 -0500

    licensing and markdown of readme complete

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,19 +1,75 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
+// Function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (!license) {
+      return ``;
+  } else {
+      return `[![${license} license](https://img.shields.io/badge/License-${license}-blue.svg)](${renderLicenseLink(license)})`
+  }
+}
 
-// TODO: Create a function that returns the license link
+// Function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license === 'MIT') {
+      return `https://www.mit.edu/~amini/LICENSE.md`
+  }
+  if (license === 'GNU') {
+      return `https://www.gnu.org/licenses/gpl-3.0.en.html`
+  }
+  if (license === 'Apache') {
+      return `https://www.apache.org/licenses/LICENSE-2.0` 
+  }
+}
 
-// TODO: Create a function that returns the license section of README
+// Function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (!license) {
+      return ``;
+  } else {
+      return `## Licenses
+      This project is covered under the ${license} license. To learn more about what this means, click the license button at the top.`
+}
+}
 
-// TODO: Create a function to generate markdown for README
+// Function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `
+  # ${data.title}
 
+  ${renderLicenseBadge(data.license)}
+
+  ## Table of Contents
+  * [Description](#description)
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [Contribution](#contribute)
+  * [Tests](#tests)
+  * [Licenses](#licenses)
+  * [Questions](#questions)
+  
+  ## Description
+  ${data.description}
+
+  ## Installation
+  ${data.installation}
+
+  ## Usage
+  ${data.usage}
+
+  ## Contribution
+  ${data.contribute}
+
+  ## Tests
+  ${data.tests}
+
+  ${renderLicenseSection(data.licenses)}
+
+  ## Questions
+  If you have any questions, please feel free to reach out!  
+  GitHub: https://github.com/${data.github}  
+  Email: ${data.email}
 `;
 }
 
